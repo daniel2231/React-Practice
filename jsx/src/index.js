f36b11813e138e7d61f1d65b0e224864cf940531
daniel2231 <kangheon2231@likelion.org>
2019-09-19 01:40:35 +0900

    TIL 2019년 09월 19일 목 오전  1:40:35

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -11,8 +11,8 @@ const App = () => {
         <div>
             <label className="label" htmlFor="name">{labelText}</label>
             <input id="name" type="text" />
-            <button style={{backgroundColor: "blue", color: "white"}}>{buttonText.text}</button>  
         </div>
+            <button style={{backgroundColor: "blue", color: "white"}}>{buttonText.text}</button>  
     );
 };
 
